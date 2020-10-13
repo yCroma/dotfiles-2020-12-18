fd4c1522b449ba2a0d55790ba8870af00911accc
Oracle Public Cloud User <opc@achevesvm.publicsubnet.achevesvcn.oraclevcn.com>
2020-10-13 02:41:14 +0000

    UPDATE: install procedure

--- init/oracle_linux/tool/tig.sh
+++ init/oracle_linux/tool/tig.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/jonas/tig.git
-cd tig
- 
-make prefix=/usr/local
-sudo make install prefix=/usr/local
+# sudo yum -y install epel-release.noarch 
+sudo yum -y install tig
+
--- init/oracle_linux/tool/vim.sh
+++ init/oracle_linux/tool/vim.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+# 参考リンク
+# https://gist.github.com/yevrah/21cdccc1dc65efd2a4712781815159fb
 
-yum install -y ncurses-devel
+################################################################################
+# Method 2: Install by building from scratch - contains a lot ot dependancies
+#
 
-git clone https://github.com/vim/vim.git
-cd vim/src
-make
+# Setup essential build environment
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install ncurses-devel git-core
+
+# Get source
+git clone https://github.com/vim/vim && cd vim
+
+# OPTIONAL: configure to provide a comprehensive vim - You can skip this step
+#  and go  straight to `make` which will configure, compile and link with
+#  defaults.
+
+./configure --prefix=/usr --with-features=huge --enable-multibyte --with-python-config-dir=/usr/lib/python2.7/config --enable-pythoninterp=yes
+
+# Build and install
+make && sudo make install
